dda669ee3e5f626654ce5397a683219a3b690da6
davidko <ksb3602@gmail.com>
2021-11-19 13:20:46 +0900

    static 파일들 엑세스 안되는 문제 해결, templates 폴더에 복사된 static 파일 삭제 및 code cleaning

--- src/main/java/com/sparta/myblog/controller/BlogController.java
+++ src/main/java/com/sparta/myblog/controller/BlogController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/")
 public class BlogController {
 
     private final BlogRepository blogRepository;
@@ -27,14 +28,14 @@ public class BlogController {
         return blogRepository.save(blog);
     }
 
-    @GetMapping("/api/blogs")
+    @GetMapping("/blogs")
     public List<Blog> getBlog() {
         LocalDateTime start = LocalDateTime.of(LocalDate.now(), LocalTime.of(0,0,0));
         LocalDateTime end = LocalDateTime.of(LocalDate.now(), LocalTime.of(23,59,59));
         return blogRepository.findAllByModifiedAtBetweenOrderByCreatedAtDesc(start, end);
     }
 
-    @GetMapping("/api/blogs/detail")
+    @GetMapping("/blogs/detail")
     public ModelAndView getOneBlog(@RequestParam Long id) {
 
         Blog blog = blogRepository.findById(id).orElseThrow(
